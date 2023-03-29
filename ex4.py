ce80446aeae1ba0fe61d6835fe66578c6880aca3
njulies <96377491+njulies@users.noreply.github.com>
2023-03-29 14:36:43 +0700

    Exercise4

---
+++ ex4.py
@@ -0,0 +1,10 @@
+foundation_eng=40
+general_bus=60
+introduction_to_com_sys=80
+computer_Pro=90
+print("--- Your Score---")
+print("Foundation English   :",foundation_eng)
+print("General Business     :",general_bus)
+print("Interoduction to Computer System : ",introduction_to_com_sys)
+print("Computer Programming : ",computer_Pro)
+print("--- End ---")
